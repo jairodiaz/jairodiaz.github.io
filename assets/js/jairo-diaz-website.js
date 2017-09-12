aa23d5df4733bb190d9e13944db866971785190b
Jorge Perez <jorge.perez@codescrum.com>
2017-09-12 17:59:15 -0500

    Fix tab navigation on What i do Section.

--- assets/js/jairo-diaz-website.js
+++ assets/js/jairo-diaz-website.js
@@ -4,7 +4,25 @@ $(document).ready(function(){
   })
 
   $('.testimonials-3col .t-item blockquote p').readmore({
-    collapsedHeight: 130
+    collapsedHeight: 130,
+    lessLink: '<small class="readmore-link">Close</small>',
+    moreLink: '<small class="readmore-link">Read more...</small>'
   });
+
+  $('#services.ft-cards .card-content-text').readmore({
+    collapsedHeight: 70,
+    lessLink: '<small class="readmore-link">Close</small>',
+    moreLink: '<small class="readmore-link">Read more...</small>'
+  });
+
+  $('.ft-cards .ft-item a[data-tab="tab1"]').click(function (e) {
+    $('.ft-tabs a[href="#tab1"]').tab('show');
+  })
+  $('.ft-cards .ft-item a[data-tab="tab2"]').click(function (e) {
+    $('.ft-tabs a[href="#tab2"]').tab('show');
+  })
+  $('.ft-cards .ft-item a[data-tab="tab3"]').click(function (e) {
+    $('.ft-tabs a[href="#tab3"]').tab('show');
+  })
 
 });
